2d324c28b3970d21ea95cbb15606dddcd7955135
KudzieChase <kudzai.chasinda@gmail.com>
2016-07-13 16:42:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/gdg/hit/events/Activities/SignInActivity.java
@@ -0,0 +1,10 @@
+package com.gdg.hit.events.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Kudzai Chasinda
+ * @author GDGHIT
+ */
+public class SignInActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/gdg/hit/events/Fragments/EventsFragment.java
@@ -0,0 +1,10 @@
+package com.gdg.hit.events.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author Kudzai Chasinda
+ * @author GDGHIT
+ */
+public class EventsFragment extends Fragment {
+}
